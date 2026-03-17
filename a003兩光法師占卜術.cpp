a5287d378599b4064d649067eeb049ecde888cc7
s1130300-dotcom <s1130300@dmhs.kh.edu.tw>
2026-03-17 11:36:22 +0800

    Create a003兩光法師占卜術.cpp

---
+++ a003兩光法師占卜術.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int M, D ,S;
+	S = (M*2+D)% 3;
+	if (S==2) {
+		cout << "大吉" << endl;
+	}else if (S==1){
+		cout << "吉" << endl;
+	}else{
+		cout << "普通" << endl;
+	}
+	return 0;
