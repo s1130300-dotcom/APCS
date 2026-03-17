a40e34ad5c1a3fca1061328f2253f94ee175bf75
s1130300-dotcom <s1130300@dmhs.kh.edu.tw>
2026-03-17 11:15:05 +0800

    Create d064奇數.cpp

---
+++ d064奇數.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i; 
+	cin >> i;
+	if ((i % 2 )==1){
+		cout << "Odd" << endl;
+	} else {
+		cout << "Even" << endl;
+	}
+	return 0;
+
