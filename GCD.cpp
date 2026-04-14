88a194ba86c6ba389fc52a33f8201b913899797a
s1130300-dotcom <s1130300@dmhs.kh.edu.tw>
2026-04-14 10:54:17 +0800

    Create GCD.cpp

---
+++ GCD.cpp
@@ -0,0 +1,13 @@
+#include <iostrean>
+using namespace std;
+
+int main() {
+	int x,y;
+	cin >> x >> y;
+	while (x % y !=0){
+		int temp =x % y;
+	}   x=y;
+	    y=temp;
+}
+cout << sum << endl;
+return 0;
