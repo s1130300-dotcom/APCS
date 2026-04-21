266c2657b78c1e87183b1700498c4461eb28fe86
s1130300-dotcom <s1130300@dmhs.kh.edu.tw>
2026-04-21 11:41:26 +0800

    Create 99table.cpp

---
+++ 99table.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     for (int i =1; i<=9; i++) {
+		for (int j =1; j <=3; i++){
+			cout <<j <<j<<"*"<<i<<"="<<;
+		}
+		cout << "\n"
+	}
+	cout << "-----------------";
+	for (int i=1; i<=9;i++){
+		for (int j=4; j <= 6; j++){
+			cout << "*";
+		}
+		cout << "\n";
+		cout << "-----------------";
+			for (int i=1; i<=9;i++){
+				for (int j=7; j <= 9; j++){
+					cout <<j <<j<<"*"<<i<<"="<<j*i<<"";
